b7a460abd759349990ff9ffe9f21cefcc7050f71
Mario Heberthardt <no@none.com>
2021-12-21 08:32:20 +0100

    First Commit

---
+++ MarvelCharactersSwiftUI/Shared/MarvelCharactersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharactersSwiftUIApp.swift
+//  Shared
+//
+//  Created by Mario Andres Heberthardt Marchant on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharactersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
